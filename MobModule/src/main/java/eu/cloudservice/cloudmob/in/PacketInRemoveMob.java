fabad8d04444abb5b8aaf2abb54fd471d68498d1
Phillipp Glanz <p.glanz@madfix.me>
2020-05-13 17:59:43 +0200

    Remove cloudnet packet and event update. Is unnecessary!! Clean up imports.

--- MobModule/src/main/java/eu/cloudservice/cloudmob/in/PacketInRemoveMob.java
+++ MobModule/src/main/java/eu/cloudservice/cloudmob/in/PacketInRemoveMob.java
@@ -1,10 +1,8 @@
 package eu.cloudservice.cloudmob.in;
 
-import de.dytanic.cloudnet.lib.network.protocol.packet.Packet;
 import de.dytanic.cloudnet.lib.network.protocol.packet.PacketInHandler;
 import de.dytanic.cloudnet.lib.network.protocol.packet.PacketSender;
 import de.dytanic.cloudnet.lib.utility.document.Document;
-import de.dytanic.cloudnetcore.CloudNet;
 import eu.cloudnetservice.cloudmob.ServerMob;
 import eu.cloudservice.cloudmob.MobModule;
 
@@ -13,7 +11,5 @@ public class PacketInRemoveMob extends PacketInHandler {
     public void handleInput(Document packet, PacketSender packetSender) {
         ServerMob serverMob = packet.getObject("mob", ServerMob.TYPE);
         MobModule.getInstance().getMobDatabase().remove(serverMob);
-        CloudNet.getInstance().getNetworkManager().reload();
-        CloudNet.getInstance().getNetworkManager().updateAll();
     }
 }
